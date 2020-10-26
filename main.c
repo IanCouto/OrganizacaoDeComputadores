ccd1486397a38ef5324a8e7078da8694b811c61d
IanCouto <48738352+IanCouto@users.noreply.github.com>
2020-10-26 20:14:15 -0300

    le arquivo

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void leArquivo()
+{
+    char file_name[25];
+    FILE *filePath;
+
+    printf("Digite o nome do arquivo de input:");
+    gets(file_name);
+
+    filePath = fopen(file_name, "r");
+
+    if (filePath == NULL)
+    {
+        perror("Arquivo nao encontrado\n");
+        exit(0);
+    }
+
+    char caractere;
+    while ((caractere = fgetc(filePath)) != EOF)
+        printf("%c", caractere);
+
+    fclose(filePath);
+}
 
 int main(int argc, char const *argv[])
 {
-    printf("teste");
+    leArquivo();
     return 0;
 }
