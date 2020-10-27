c06453c07be7b4a38cc4241b2c3874a716678f30
RubioKing <matheus.rubio@ice.ufjf.br>
2020-10-26 21:16:38 -0300

    Leitura de Arquivo

--- main.c
+++ main.c
@@ -2,9 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-void leArquivo()
+
+
+char **leArquivo()
 {
     FILE *filePath;
+    char **mat;
+    char tam = 1;
+    mat = (char**)malloc(sizeof(char) * tam);
+
+    for (int i = 0; i < 16; i++)
+    {
+        mat[i] = (char*)malloc(sizeof(char) * 4);
+    }
 
     filePath = fopen("input.txt", "r");
 
@@ -14,22 +24,33 @@ void leArquivo()
         exit(0);
     }
 
-    char caractere;
-    while ((caractere = fgetc(filePath)) != EOF)
-        printf("%c", caractere);
+    /*
+    
+    printf("\n");
 
+    for (int i = 0; mat[i][0] != EOF; i++)
+    {
+        fgets(mat[i], 5, filePath);
+        printf(mat[i]);
+    }
     fclose(filePath);
+    
+    */
+    return mat;
 }
 
-void menu(){
+
+void menu()
+{
     printf("[0]carga do arquivo (fornecer o nome do arquivo)\n");
     printf("[1]entrada do programa via teclado (forma alternativa), evitando a necessidade de leitura do arquivo\n");
     printf("[2]inicio da execucao (passo-a-passo ou direta)\n");
-    printf("[3]reset - utilizado para limpar a memória/registradores\n");
+    printf("[3]reset - utilizado para limpar a memoria/registradores\n");
 }
 
 int main(int argc, char const *argv[])
 {
+    menu();
     leArquivo();
     return 0;
 }
